37290adfe1e4812fd5d37aa181cf64e12b1d14a0
frmnxhken <frmnxhken@gmail.com>
2025-10-06 01:12:27 +0800

    Refacto all page

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,9 @@
 Harap jangan dirubah-rubah file ini.
 
 */
+const toggleBtn = document.getElementById("toggle-menu");
+const navMobileLink = document.querySelector(".navbar-mobile-links");
+toggleBtn.addEventListener("click", () => {
+  toggleBtn.classList.toggle("active");
+  navMobileLink.classList.toggle("active");
+});
